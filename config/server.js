a96d9540ec22d08b329e080b6b3456d7c75d81d8
Ayenson, Mika <mika.ayenson@jhuapl.edu>
2021-11-01 18:48:23 -0500

    add crm

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'baf40635a3e53266bb119d32f16cdc09'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9d0e764-f95f-46f2-a407-3b949dc554df'
+};
