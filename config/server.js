14c59c5966fb24b22ac852eb12abd0a3d68869ac
Ayenson, Mika <mika.ayenson@jhuapl.edu>
2021-11-01 20:28:49 -0500

    remove no longer needed dev JWT token

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'baf40635a3e53266bb119d32f16cdc09'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
